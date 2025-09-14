af718b78155251201f0ea0a18148e29f8ccfebde
JulioMourajr <juliomourajr@hotmail.com>
2025-09-14 02:52:37 -0300

    refactor: aplicando o terraform fmt.

--- Infra/modules.tf
+++ Infra/modules.tf
@@ -3,7 +3,7 @@ module "rede" {
 }
 
 module "clusterk8s" {
-  source = "./modules/clusterk8s"
+  source              = "./modules/clusterk8s"
   publica_subnet_a_id = module.rede.aws_subnet_publica_a_id
   publica_subnet_b_id = module.rede.aws_subnet_publica_b_id
 }
